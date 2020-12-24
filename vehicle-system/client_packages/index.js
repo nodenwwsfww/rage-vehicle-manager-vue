8bc3d8857d495fae10d45cf993ce8b90a76ef874
Roman Radchenko <nodenwwsfww@gmail.com>
2020-12-24 15:32:01 +0300

    remove trash files

--- vehicle-system/client_packages/index.js
+++
@@ -1,5 +0,0 @@
-global.player = mp.players.local;
-
-// Browser
-require('freeroam/browsers/browserAPI');
-require('freeroam/index.js');
--- vehicle-system/vue-cef/src/views/Home.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-        <h2>Home page</h2>
-        <a href="./hud">Hud</a>
-    </div>
-</template>
